3ac6f42b58dac07f9da401a8ec6f9e3ef2c67c23
Руслан Халилов <xalilove@MacBook-Pro-Ruslan.local>
2022-11-24 00:36:34 +0300

    parsingRoman

--- calculate.go
+++ calculate.go
@@ -28,9 +28,56 @@ func parseInt(x, y string) (int, int) {
 	return num1, num2
 }
 
+func romanToInt(s string) int {
+	var v, lv, cv int
+	h := map[uint8]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		cv = h[s[i]]
+		if cv < lv {
+			v -= cv
+		} else {
+			v += cv
+		}
+		lv = cv
+	}
+
+	return v
+}
+
+func Roman(number int) string {
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	roman := ""
+	for _, conversion := range conversions {
+		for number >= conversion.value {
+			roman += conversion.digit
+			number -= conversion.value
+		}
+	}
+	return roman
+}
+
 func main() {
 	var x, plus, y string
 	fmt.Scanln(&x, &plus, &y)
+	if x == "I" && y == "II" {
+		fir := romanToInt(x) + romanToInt(y)
+		fmt.Println(Roman(fir))
+	}
 	num1, num2 := parseInt(x, y)
 	switch plus {
 	case "+":
